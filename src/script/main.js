eeab68fb32c09ddab3bbd19436b480c49be5b86f
Oskar Sundström <ossu2300@student.miun.se>
2024-02-25 21:58:47 +0100

    added ny mapp för script filer

---
+++ src/script/main.js
@@ -0,0 +1,20 @@
+"use strict"
+
+
+    document.getElementById("buttonAnimation").addEventListener("click", function() { 
+    document.getElementById("bil1Animation").classList.toggle("bil1AfterClick"); 
+    document.getElementById("bil2Animation").classList.toggle("bil2AfterClick");
+
+});
+
+
+document.getElementById("buttonAnimation").addEventListener("click", function() {
+    document.getElementById("buttonAnimation").classList.toggle("spinningOnClick");
+    
+})
+
+
+
+    
+
+                 
